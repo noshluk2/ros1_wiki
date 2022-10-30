67c22dbd27c986541808d7d6f1eee8d6a1bfa2b9
Luqman <mluqman@ur-techpartner.de>
2022-10-30 18:27:38 +0500

    #11 Linux gui docker file added

---
+++ docker/linux/run_docker_gpu.bash
@@ -0,0 +1,24 @@
+# If not working, first do: sudo rm -rf /tmp/.docker.xauth
+# It still not working, try running the script as root.
+## Build the image first
+### docker build -t r2_path_planning .
+## then run this script
+xhost local:root
+
+
+XAUTH=/tmp/.docker.xauth
+
+docker run -it \
+    --name=r2_pathplanning_container \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+    --net=host \
+    --privileged \
+    --runtime=nvidia \
+    haiderabbasi333/ros2-pathplanning-course:1 \
+    bash
+
+echo "Done."
